abfe113319550c109cffa03e45b4d7677b4b0684
Sarah Binesmael <sbinesmael@gmail.com>
2024-02-17 23:40:14 +0000

    updated sign in page styles

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 import {
   StyleSheet,
   View,
@@ -8,14 +8,16 @@ import {
   Keyboard,
   ActivityIndicator,
   KeyboardAvoidingView,
-} from "react-native";
-import TouchableScale from "react-native-touchable-scale";
-import { FIREBASE_AUTH } from "../../firebaseConfig";
-import { signInWithEmailAndPassword } from "firebase/auth";
+} from 'react-native';
+import TouchableScale from 'react-native-touchable-scale';
+import { FIREBASE_AUTH } from '../../firebaseConfig';
+import { signInWithEmailAndPassword } from 'firebase/auth';
+import { Ionicons } from '@expo/vector-icons'; // Make sure to have this installed
+import { Image, /* other components */ } from 'react-native';
 
 const LoginScreen = ({ navigation }) => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
   const [loading, setLoading] = useState(false);
 
   const auth = FIREBASE_AUTH;
@@ -27,64 +29,81 @@ const LoginScreen = ({ navigation }) => {
       console.log(response);
     } catch (error) {
       console.log(error);
-      alert("Sign up failed: " + error.message);
+      alert('Sign up failed: ' + error.message);
     } finally {
       setLoading(false);
     }
   };
 
   return (
-    <KeyboardAvoidingView style={{ flex: 1 }} behavior="padding">
-      <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
-        <View style={styles.container}>
-          <View style={styles.contentContainer}>
-            <Text style={styles.title}>Sign In To Pool</Text>
-            <TextInput
-              style={styles.input}
-              placeholder="Email"
-              value={email}
-              onChangeText={setEmail}
-              keyboardType="email-address"
-            />
-            <TextInput
-              style={styles.input}
-              placeholder="Password"
-              value={password}
-              onChangeText={setPassword}
-              secureTextEntry
-            />
+    <KeyboardAvoidingView style={{ flex: 1 }} behavior='padding'>
+  <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
+    <View style={styles.container}>
+      {/* Move the Image component here, outside the contentContainer */}
+      <Image
+        source={require('../../assets/app-icon.png')}
+        style={styles.logo}
+      />
+
+        <Text style={styles.title}>Sign In To Pool</Text>
+        <TextInput
+          style={styles.input}
+          placeholder='Email'
+          value={email}
+          onChangeText={setEmail}
+          keyboardType='email-address'
+        />
+        <TextInput
+          style={styles.input}
+          placeholder='Password'
+          value={password}
+          onChangeText={setPassword}
+          secureTextEntry
+        />
 
             {loading ? (
-              <ActivityIndicator size="large" color="#FFFFFF" />
+              <ActivityIndicator size='large' color='#FFFFFF' />
             ) : (
-              <TouchableScale
-                style={styles.buttonContainer}
-                onPress={handleLogin}
-              >
+              <TouchableScale style={styles.buttonContainer} onPress={handleLogin}>
                 <Text style={styles.buttonText}>Login</Text>
               </TouchableScale>
             )}
 
-            <TouchableScale onPress={() => navigation.navigate("SignUp")}>
-              <Text style={styles.switchText}>
-                Don't have an account? Sign Up
-              </Text>
-            </TouchableScale>
+            {/* Social Media Login Section */}
+            <Text style={styles.socialLoginText}>Or, login with...</Text>
+
+            <View style={styles.socialIconsContainer}>
+  <TouchableScale onPress={() => {}}>
+    <View style={styles.iconWrapper}>
+      <Ionicons name="logo-google" size={32} color="#FFFFFF" />
+    </View>
+  </TouchableScale>
+  <TouchableScale onPress={() => {}}>
+    <View style={styles.iconWrapper}>
+      <Ionicons name="logo-facebook" size={32} color="#FFFFFF" />
+    </View>
+  </TouchableScale>
+  <TouchableScale onPress={() => {}}>
+    <View style={styles.iconWrapper}>
+      <Ionicons name="logo-twitter" size={32} color="#FFFFFF" />
+    </View>
+  </TouchableScale>
+</View>
+
+
+            <TouchableScale onPress={() => navigation.navigate('SignUp')}>
+  <Text style={styles.switchText}>
+    Don't have an account? <Text style={styles.boldText}>Sign Up</Text>
+  </Text>
+</TouchableScale>
           </View>
-        </View>
       </TouchableWithoutFeedback>
     </KeyboardAvoidingView>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#022D3B",
-    padding: 20,
-  },
+  
   contentContainer: {
     width: "100%", // Ensure the container takes the full width
     flex: 1,
@@ -103,17 +122,17 @@ const styles = StyleSheet.create({
     paddingVertical: 12,
     paddingHorizontal: 20,
     backgroundColor: "#FFFFFF",
-    borderRadius: 25,
+    borderRadius: 10,
     fontSize: 16,
     color: "#333",
     elevation: 6,
   },
   buttonContainer: {
-    marginTop: 20,
-    width: "40%",
-    backgroundColor: "#2ECC40",
-    borderRadius: 25,
-    padding: 15,
+    marginTop: 12,
+    width: "90%",
+    backgroundColor: "#1BA77C",
+    borderRadius: 10,
+    padding: 12,
     justifyContent: "center",
     alignItems: "center",
     elevation: 7,
@@ -132,6 +151,85 @@ const styles = StyleSheet.create({
     color: "#FFFFFF",
     fontSize: 16,
   },
+
+  socialLoginText: {
+    marginTop: 20,
+    color: '#FFFFFF',
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 40, // Increase this value to create a larger gap
+  },
+  socialIconsContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+    alignItems: 'center',
+    width: '80%',
+    marginTop: 20, // Optionally, add marginTop here if additional spacing is needed
+    marginBottom: 30, // Adjust if you want to change the distance to the next element
+  },
+  iconWrapper: {
+    borderWidth: 1,
+    borderColor: '#FFFFFF',
+    borderRadius: 15, // Adjust for more pronounced squoval corners if needed
+    padding: 10, // Adjust padding as needed for size
+    marginLeft: 10, // Add spacing between icons if needed
+    marginRight: 10, // Add spacing between icons if needed
+  },
+
+  socialIcon: {
+    padding: 10, // Add padding to increase the touchable area
+  },
+
+  logoContainer: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: '5%', // Adjust the top margin as needed
+    marginBottom: '5%', // Adjust the bottom margin as needed
+  },
+  container: {
+    flex: 1,
+    justifyContent: "flex-start", // Align items to the start of the container
+    alignItems: "center",
+    backgroundColor: "#022D3B",
+    paddingTop: 20, // Adjust this to control space at the top
+    paddingHorizontal: 20,
+  },
+  logo: {
+    height: 100,
+    width: 100,
+    resizeMode: 'contain',
+    marginTop: 70, // Adjust this to control space at the top around the logo
+    marginBottom: 70, // Adjust space between the logo and the contentContainer
+  },
+  
+
+  socialIconsContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+    alignItems: 'center',
+    width: '80%',
+    marginBottom: 30, // Increased to lower the icons
+  },
+  iconWrapper: {
+    borderWidth: 1, // Thin border
+    borderColor: '#FFFFFF', // White border color
+    borderRadius: 10, // Adjust for squoval shape
+    padding: 8, // Padding inside the border
+  },
+
+  switchText: {
+    marginTop: 25,
+    color: "#FFFFFF",
+    fontSize: 16,
+  },
+  boldText: {
+    fontWeight: 'bold',
+    color: "#FFFFFF", // Ensure the color matches if needed
+    // You can inherit the fontSize from switchText or define it again if necessary
+  },
+
+
+
 });
 
 export default LoginScreen;
