5c6e895817814110fa62907927fb4f09082fdd03
MaratD87 <maratdanyarov87@gmail.com>
2024-02-17 20:08:36 +0000

    create investmentPool file

--- src/utilities/User.js
+++
@@ -1,8 +0,0 @@
-class User {
-    constructor(id, name, email, balance = 0) {
-        this.id = id;
-        this.name = name;
-        this.email = email;
-        this.balance = balance;
-    }
-}
