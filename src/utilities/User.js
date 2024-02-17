7d16848727c62f4abe34a4bae5476021bd9666b2
MaratD87 <maratdanyarov87@gmail.com>
2024-02-17 19:09:31 +0000

    create User class

---
+++ src/utilities/User.js
@@ -0,0 +1,8 @@
+class User {
+    constructor(id, name, email, balance = 0) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.balance = balance;
+    }
+}
