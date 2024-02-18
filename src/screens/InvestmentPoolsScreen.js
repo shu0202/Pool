9bf85d94d45b58accda86cce476e2047c7f5b920
Ivan Yeung <91327331+shu0202@users.noreply.github.com>
2024-02-18 02:51:31 +0000

    added pools owned button

--- src/screens/InvestmentPoolsScreen.js
+++ src/screens/InvestmentPoolsScreen.js
@@ -1,11 +1,59 @@
 import React, { useState, useEffect } from "react";
-import { ScrollView, View, Text, StyleSheet } from "react-native";
-import { collection, getDocs } from "firebase/firestore";
-import { FIREBASE_DB } from "../../firebaseConfig"; // Adjust this path as necessary
-import Header from "../components/AppHeader";
+import { ScrollView, View, Text, StyleSheet, Modal, TextInput, TouchableOpacity, TouchableWithoutFeedback, Keyboard, Dimensions  } from "react-native";
+import Header from "../components/AppHeader"; // Adjust the import path as necessary
+import { addDoc, getDocs, collection, doc, getDoc } from "firebase/firestore";
+import { FIREBASE_DB, FIREBASE_AUTH } from "../../firebaseConfig";
 
 const InvestmentPools = () => {
+  const [modalVisible, setModalVisible] = useState(false);
+  const [name, setName] = useState('');
+  const [amount, setAmount] = useState('');
+  const [paytime, setPaytime] = useState('');
+  const [interest, setInterest] = useState('');
   const [pools, setPools] = useState([]); // State to hold fetched pools
+
+  const headerOptions = {
+    right: [
+      {
+        icon: "add-circle",
+        onPress: () => setModalVisible(true),
+      },
+    ],
+  };
+
+  const handleSave = async () => {
+    const user = FIREBASE_AUTH.currentUser;
+    if (user) {
+      try {
+        const newPool = {
+          name,
+          amount,
+          paybacktime: paytime,
+          creatorId: user.uid, // Include the ID of the current user
+        };
+
+        const docRef = await addDoc(
+          collection(FIREBASE_DB, "FriendlyPools"),
+          newPool
+        );
+        console.log("Document written with ID: ", docRef.id);
+
+        setName("");
+        setAmount("");
+        setPaytime("");
+        setInterest("");
+        setModalVisible(false);
+      } catch (error) {
+        console.error("Error adding document: ", error);
+      }
+    } else {
+      console.error("No user logged in");
+    }
+};
+
+const dismissKeyboard = () => {
+  Keyboard.dismiss();
+};
 
   useEffect(() => {
     const fetchPools = async () => {
@@ -29,19 +77,32 @@ const InvestmentPools = () => {
     fetchPools();
   }, []);
 
-  const headerOptions = {
-    right: [
-      {
-        icon: "add-circle",
-        onPress: () => console.log("Add Icon Pressed"),
-      },
-    ],
-  };
 
   return (
     <View style={styles.pageContainer}>
       <Header options={headerOptions} />
       <ScrollView contentContainerStyle={styles.contentContainer}>
+      <View style={styles.poolsOwned}>
+          <Text style={styles.sectionTitle}>Pools Owned</Text>
+          <ScrollView
+            horizontal
+            showsHorizontalScrollIndicator={true}
+            contentContainerStyle={styles.horizontalScroll}
+          >
+            {pools.map((pool) => (
+              <View key={pool.id} style={styles.pool}>
+                <Text style={styles.poolText}>Name: {pool.name}</Text>
+                <Text style={styles.poolText}>Amount: {pool.amount}</Text>
+                <Text style={styles.poolText}>
+                  Payback Time: {pool.paybacktime} days
+                </Text>
+                <Text style={styles.poolText}>
+                  Pool Creator: {pool.creatorName}
+                </Text>
+              </View>
+            ))}
+          </ScrollView>
+        </View>
         <View style={styles.poolCategories}>
           <Text style={styles.sectionTitle}>Pool Categories</Text>
         </View>
@@ -76,32 +137,38 @@ const styles = StyleSheet.create({
     color: "#FFF",
     marginBottom: 10,
   },
-  poolCategories: {
+  poolsOwned: {
     backgroundColor: "rgba(255, 255, 255, 0.8)",
     padding: 20,
     borderRadius: 20,
     marginBottom: 20,
   },
-  myInvestments: {
+  poolCategories: {
     backgroundColor: "rgba(255, 255, 255, 0.7)",
     padding: 20,
     borderRadius: 20,
     marginBottom: 20,
   },
-  performanceAnalytics: {
+  myInvestments: {
     backgroundColor: "rgba(255, 255, 255, 0.6)",
     padding: 20,
     borderRadius: 20,
     marginBottom: 20,
   },
-  recommendedPools: {
+  performanceAnalytics: {
     backgroundColor: "rgba(255, 255, 255, 0.5)",
     padding: 20,
     borderRadius: 20,
     marginBottom: 20,
   },
-  legalDocuments: {
+  recommendedPools: {
     backgroundColor: "rgba(255, 255, 255, 0.4)",
+    padding: 20,
+    borderRadius: 20,
+    marginBottom: 20,
+  },
+  legalDocuments: {
+    backgroundColor: "rgba(255, 255, 255, 0.3)",
     padding: 20,
     borderRadius: 20,
     marginBottom: 20,
